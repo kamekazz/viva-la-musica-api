9e08e0213b7e67119e217783c626cf1fad7d0314
kamekazz <taveras78@gmail.com>
2018-11-26 03:26:40 -0800

    si

---
+++ conf/confing.js
@@ -0,0 +1,8 @@
+const confing = {
+    dbUser: 'kamekazz',
+    dbPassword:'titolovo76',
+    secret:'qwertyuiop ',
+    API_KEY :'AIzaSyD0JpBJqOZVZIg-1sWtNwVE7KHSwVxtSj8'
+}
+
+module.exports = confing
